5862ee3f03499f4eb39d4ed08f53ac226ef92452
Linda <lindasandy2835@gmail.com>
2021-01-02 21:11:28 +0800

    分開1

--- JS.js
+++ JS.js
@@ -1,39 +1,36 @@
 
-            $(document).ready(function(){
-            $("a").hover(function(){
-                $("p").hide();
-            }, function(){
-                $("p").show();
-            });
-            });
+$(document).ready(function(){
+    $("a").hover(function(){
+        $("p").hide();
+    }, function(){
+    $("p").show();
+    });
+});
         
         
         
-            x=1
-            cache=""
-            function change(){
-              cache+="<img src='https://i.ibb.co/6Pnr8K9/black.png' draggable='true' id='picture_1_${x}'>"; 
-              x++
-              document.getElementById("photo").innerHTML=cache;
-            }
+x=1
+cache=""
+function change(){
+    cache+="<img src='https://i.ibb.co/6Pnr8K9/black.png' draggable='true' id='picture_1_${x}'>"; 
+    x++
+document.getElementById("photo").innerHTML=cache;
+}
     
 
 
-    document.addEventListener("dragstart", function(event) {
-        event.dataTransfer.setData("Text", event.target.id);
-        
+document.addEventListener("dragstart", function(event) {
+    event.dataTransfer.setData("Text", event.target.id);
     });
-    document.addEventListener("dragend", function(event) {
-        
-    });
+
     /* 拖動完成後觸發*/
-    document.addEventListener("dragover", function(event) {
-        event.preventDefault();
-    });
-    document.addEventListener("drop", function(event) {
-        event.preventDefault();
-        if ( event.target.className == "droptarget" ) {
-            var data = event.dataTransfer.getData("Text");
-            event.target.appendChild(document.getElementById(data));
-        }
-    });
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    if ( event.target.className == "droptarget" ) {
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
+});
