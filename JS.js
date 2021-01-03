407a3af0dbedd5201e74e88eaaa59a5122aa6807
Linda <lindasandy2835@gmail.com>
2021-01-04 04:19:36 +0800

    aewtreyr

--- JS.js
+++ JS.js
@@ -1,45 +1,87 @@
-$(document).ready(function(){
-    $("a").hover(function(){
+document.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("Text", event.target.id);
+});
+
+/* ��ʧ�����Ĳ�o*/
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+});
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.className == "droptarget") {
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
+});
+$(document).ready(function () {
+    $("a").hover(function () {
         $("p").hide();
-    }, function(){
-    $("p").show();
+    }, function () {
+        $("p").show();
     });
 });
-        
-        
-        
 
 
-function showImage(){
-    var image =new Image();
-    image.src="https://i.ibb.co/6Pnr8K9/black.png"
-    image.draggable=true
-    document.getElementById("show").appendChild(image)
+
+x = 1
+cache = ""
+function change() {
+    cache += "<img src='https://i.ibb.co/6Pnr8K9/black.png' draggable='true' id='picture_1_${x}'>";
+    x++
+    document.getElementById("photo").innerHTML = cache;
+}
+function clearBox(elementID) {
+    document.getElementById(elementID).innerHTML = "";
+    var cleardot = document.getElementById('picture_1_${x}');
+    cleardot.remove();
+    x = 0;
+}
+function displayelement() {
+    var image1 = document.getElementById("img1");
+    image1.src = "https://i.imgur.com/LTvUEfi.png";
+    var image2 = document.getElementById("img2");
+    image2.src = "https://i.imgur.com/Z38gXxq.png";
+    var image3 = document.getElementById("img3");
+    image3.src = "https://i.imgur.com/PQCiA6b.png";
+    var image4 = document.getElementById("img4");
+    image4.src = "https://i.imgur.com/zC0PWQ0.png";
+    var image5 = document.getElementById("img5");
+    image5.src = "https://i.imgur.com/oIuoMUY.png";
+    var image6 = document.getElementById("img6");
+    image6.src = "https://i.imgur.com/BUrLAMc.png";
+    var image7 = document.getElementById("img7");
+    image7.src = "https://i.imgur.com/YHyUArl.png";
+    var image8 = document.getElementById("img8");
+    image8.src = "https://i.imgur.com/tpuH3HK.png";
+    var image9 = document.getElementById("img9");
+    image9.src = "https://i.imgur.com/QtiP2fC.png";
+    var image10 = document.getElementById("img10");
+    image10.src = "https://i.imgur.com/HM60tcC.png";
 
 }
-    
+function createtxt() {
+    // First create a DIV element.
+    var txtNewInputBox = document.createElement('div');
+    // Then add the content (a new input box) of the element.
+    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox'>";
 
-var stage = new Konva.Stage({
-    container:"container",
-    width:window.innerWidth,
-    heigh:window.innerHeight
+    // Finally put it where it is supposed to appear.
+   
+    document.getElementById("photo").appendChild(txtNewInputBox);
+}
+document.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("Text", event.target.id);
 });
 
-var layer = new Konva.Layer();
-layer.add(image)
-stage.add(layer)
-
-
-var stage = new Konva.Stage({
-    container: 'container', //容器id
-    width: window.innerWidth,// canvas宽度
-    height: window.innerHeight// canvas高度
-  });
-  
-  var layer = new Konva.Layer();
-  stage.add(layer);
-
-
-
-
+/* ��ʧ�����Ĳ�o*/
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+});
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.className == "droptarget") {
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
+});
 
